07eb760e40994ab751953296a286b22ae242b9df
algalyq <algalyq@mail.ru>
2023-03-24 14:41:35 +0600

    Error with consumers fixed

--- app/consumers.py
+++ app/consumers.py
@@ -6,6 +6,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name =self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+
         await self.channel_layer.group_add ( 
             self.room_group_name,
             self.channel_name
@@ -24,13 +25,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def tester_message(self,event):
         tester = event['tester']
 
-        await self.send(text_data=json.dumps({'tester':tester}))
+        await self.send(text_data=json.dumps({'tester':tester,}))
     
-    async def disconnect(self, code):
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        
     pass
     # def connect(self):
     #     self.room_group_name = 'test'
