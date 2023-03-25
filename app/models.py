f5cfe0b4e192a0b3711d0f50084d6d75a688347d
algalyq <algalyq@mail.ru>
2023-03-25 22:43:20 +0600

    Message model added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    UserId = models.ForeignKey(User, on_delete=models.CASCADE,related_name="sendUser")
+    ToUser = models.ForeignKey(User, on_delete=models.CASCADE,related_name="toUser")
+    Timest = models.TimeField()
+    Msg = models.TextField(max_length=1000)
--- app/views.py
+++ app/views.py
@@ -10,3 +10,4 @@ def room(request,room_name):
     return render(request,'app/room.html',{
         'room_name':room_name
     })
+
